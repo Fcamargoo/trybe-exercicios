aef290768501ae2caf31deb3ce1af55d61561be8
Fcamargoo <feecamargoo12@gmail.com>
2022-07-11 18:33:53 -0300

    adicionados arquivos para exercicio pt2 flexbox e forms

---
+++ Flexbox e forms/fundamentos/dia-4-css-flexbox-parte-2/script.js
@@ -0,0 +1,23 @@
+// const button = document.querySelector("#btn-enviar");
+// let inputNameUser = document.querySelector("#inputNameUser");
+// const nameUser = document.querySelector("#nameUser");
+
+// function getUserValue(event){
+//     event.preventDefault();
+//     console.log(inputNameUser.value);
+//     nameUser.innerHTML = inputNameUser.value;
+// }
+// button.addEventListener('click', getUserValue);
+
+// No codigo abaixo eu criei uma variavel(let) "peguei" meu formulario com um seletor, adicionei um escutador ao evento "submit" juntamente de uma função especificando que eu queria que o botão de "submit" parasse de emitir o comportamento padrao adicionando o "preventDefault".
+
+// let form = document.querySelector('form');
+// form.addEventListener('submit', function(event){
+//     event.preventDefault();
+//     if (document.getElementById('selectArea1').checked === false){
+//   event.preventDefault();
+//   console.log('Não envia');
+//     }
+//   console.log(event);  
+// })
+
